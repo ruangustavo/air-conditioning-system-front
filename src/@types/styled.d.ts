1e4de7f1f83224114aaa4bf6d30873ddd2c6db46
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-04-17 19:59:56 -0300

    fix: props not recognized

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { defaultTheme } from "../styles/theme";
+import { defaultTheme } from "../styles/themes/default";
 
 type ThemeType = typeof defaultTheme;
 
